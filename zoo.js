b237c2463fabfb500c0db56bc1618b21fa143727
Whitney <bdwseybold@gmail.com>
2016-10-06 11:03:05 -0500

    added characters

--- zoo.js
+++ zoo.js
@@ -1,1 +1,9 @@
-var 
+function character(){
+this.health = 100;
+getHealth =>{return this.health}
+}
+
+var player 1 = new janitor ("");
+var player 2 = new punk ("");
+var player 3 = new burgler ("");
+
