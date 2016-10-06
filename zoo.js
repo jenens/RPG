3943cb34a7745780bd71ccd61e1b4a32019837bd
Whitney <bdwseybold@gmail.com>
2016-10-06 15:14:43 -0500

    css adjustments

--- zoo.js
+++ zoo.js
@@ -1,34 +1,11 @@
-function character(type){
+function character(){
 this.health = 100;
 getHealth =>{return this.health}
-this.banana = 1;
-getBanana =>{return this.banana}
 }
 
-
-function chooseCharacter(){
-var player = ()
-if (type==="burgler") {
-	player=new burgler()
-}
-
-else if (type==="janitor"){
-	player=new janitor()
-}
-
-else if (type==="punk") {
-	player=new punk()
-}
+var player 1 = new janitor ("");
+var player 2 = new punk ("");
+var player 3 = new burgler ("");
 
 
-return player;
-
-function getBanana(){
-	this.score = +1;
-	getScore =>{return this.score}
-}
-
-// var player 1 = new janitor ("");
-// var player 2 = new punk ("");
-// var player 3 = new burgler ("");
-
+document.getDocumentById("page2");
