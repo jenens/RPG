dff25f5243035a2cea926bdf5fbc8067c91823f4
Jen Ensslin <jenens1@gmail.com>
2016-10-06 12:10:15 -0500

    added functions for buttons

--- zoo.js
+++ zoo.js
@@ -1,9 +1,34 @@
-function character(){
+function character(type){
 this.health = 100;
 getHealth =>{return this.health}
+this.banana = 1;
+getBanana =>{return this.banana}
 }
 
-var player 1 = new janitor ("");
-var player 2 = new punk ("");
-var player 3 = new burgler ("");
+
+function chooseCharacter(){
+var player = ()
+if (type==="burgler") {
+	player=new burgler()
+}
+
+else if (type==="janitor"){
+	player=new janitor()
+}
+
+else if (type==="punk") {
+	player=new punk()
+}
+
+
+return player;
+
+function getBanana(){
+	this.score = +1;
+	getScore =>{return this.score}
+}
+
+// var player 1 = new janitor ("");
+// var player 2 = new punk ("");
+// var player 3 = new burgler ("");
 
