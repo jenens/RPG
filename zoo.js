fd5c8aa9ba1459b9b043a052b7ba95ce698f6e1a
Whitney <bdwseybold@gmail.com>
2016-10-06 17:26:50 -0500

    edited code and UI

--- zoo.js
+++ zoo.js
@@ -10,9 +10,7 @@ this.health = 100;
 getHealth =>{return this.health}
 }
 
-var player 1 = new janitor ("");
-var player 2 = new punk ("");
-var player 3 = new burgler ("");
+
 
 function chooseCharacter(type){
 document.getElementById("background").innerHTML="<h1>THIS WORKS!!!!!!!!!!!!</h1>";
@@ -24,14 +22,15 @@ if (type==="burgler") {
 }
 
 else if (type==="janitor"){
-	player=new janitor();
 	console.log("THIS Janitor WORKS");
+	player=new janitor();
+
 	
 }
 
 else if (type==="punk") {
-	player=new punk();
 	console.log("THIS Punk WORKS");
+	player=new punk();
 	
 }
 
