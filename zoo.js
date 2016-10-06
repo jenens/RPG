c8d32cae3bffc7a33a36e964ab6fbf8ce1ad9148
Whitney <bdwseybold@gmail.com>
2016-10-06 16:29:55 -0500

    merge conflict

--- zoo.js
+++ zoo.js
@@ -1,3 +1,10 @@
+var dataObject = [
+	
+]
+
+
+
+
 function character(){
 this.health = 100;
 getHealth =>{return this.health}
@@ -7,44 +14,5 @@ var player 1 = new janitor ("");
 var player 2 = new punk ("");
 var player 3 = new burgler ("");
 
-<<<<<<< HEAD
 
-document.getDocumentById("page2");
-=======
-function chooseCharacter(type){
-document.getElementById("background").innerHTML="<h1>THIS WORKS!!!!!!!!!!!!</h1>";
-console.log("THIS THING WORKS");
-var player;
-if (type==="burgler") {
-	player=new burgler();
-	
-}
-
-else if (type==="janitor"){
-	player=new janitor();
-	
-}
-
-else if (type==="punk") {
-	player=new punk();
-	
-}
-
-
-return player;
-
-function getBanana(){
-	this.score = +1;
-	getScore =>{return this.score}
-}
-}
-
-// var player 1 = new janitor ("");
-// var player 2 = new punk ("");
-// var player 3 = new burgler ("");
-
- // document.getElementById("page2").src = "camel.png";
-
- // document.getElementById(content.style.display none)
-
->>>>>>> 9007b01ba799232b69d8276f8c75620e45e406c3
+// document.getDocumentById("page2");
