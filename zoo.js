9007b01ba799232b69d8276f8c75620e45e406c3
Jen Ensslin <jenens1@gmail.com>
2016-10-06 15:14:01 -0500

    added functions to characters

--- zoo.js
+++ zoo.js
@@ -6,18 +6,23 @@ getBanana =>{return this.banana}
 }
 
 
-function chooseCharacter(){
-var player = ()
+function chooseCharacter(type){
+document.getElementById("background").innerHTML="<h1>THIS WORKS!!!!!!!!!!!!</h1>";
+console.log("THIS THING WORKS");
+var player;
 if (type==="burgler") {
-	player=new burgler()
+	player=new burgler();
+	
 }
 
 else if (type==="janitor"){
-	player=new janitor()
+	player=new janitor();
+	
 }
 
 else if (type==="punk") {
-	player=new punk()
+	player=new punk();
+	
 }
 
 
@@ -27,8 +32,13 @@ function getBanana(){
 	this.score = +1;
 	getScore =>{return this.score}
 }
+}
 
 // var player 1 = new janitor ("");
 // var player 2 = new punk ("");
 // var player 3 = new burgler ("");
+
+ // document.getElementById("page2").src = "camel.png";
+
+ // document.getElementById(content.style.display none)
 
