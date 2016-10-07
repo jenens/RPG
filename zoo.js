214933bb374c52e676e986c1ad1d7e0582b6b5f9
Jen Ensslin <jenens1@gmail.com>
2016-10-07 08:02:41 -0500

    added inheritance to players

--- zoo.js
+++ zoo.js
@@ -3,11 +3,39 @@ var dataObject = [
 ]
 
 function character(){
-this.health = 100;
-getHealth =>{return this.health}
+var health = 100;
+this.getHealth = function(){return health};
+this.banana = 1;
+getBanana =>{return this.banana}
 }
 
+// burglar, punk and janior inherit from character function
 
+function burgler(color){
+	this.mask = color;
+	this.runningshoes = true;
+}
+
+function punk(color){
+	this.hair = color;
+	this.radio = true;
+}
+
+function janitor(color){
+	this.mop = color;
+	this.cleaningcart = true;
+}
+
+burgler.prototype=new character();
+punk.prototype=new character();
+janitor.prototype=new character();
+
+var player1 = new punk("pink");
+var player2 = new janitor("blue");
+var player3 = new burgler("black");
+
+
+// Choose Character function has else/if statements
 
 function chooseCharacter(type){
 document.getElementById("background").innerHTML="<h1></h1>";
